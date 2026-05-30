cfb38180b00f43a949bed20ede8d840d0012d1c9
Kelvin <kelvinkipchumba935@gmail.com>
2026-05-30 19:00:54 +0300

    Fix: server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB  = require('./config/db');
-const postRouter = require('./routes/blogRoutes');
+const postRouter = require('./routes/postRoutes');
 
 // Load environment variables first
 dotenv.config();
